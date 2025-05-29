1cdd3e2432c46da21520254d33ae86e879cad0a1
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-05-29 23:47:42 +0200

    fix: add missing CRD moves for kubernetes job, role, binding, and service account

--- migrations.tf
+++ migrations.tf
@@ -14,6 +14,26 @@ moved {
 }
 
 moved {
+  from = kubernetes_job.crds_helm_argo_application_wait
+  to   = module.crds.kubernetes_job.helm_argo_application_wait
+}
+
+moved {
+  from = kubernetes_role.crds_helm_argo_application_wait
+  to   = module.crds.kubernetes_role.helm_argo_application_wait
+}
+
+moved {
+  from = kubernetes_role_binding.crds_helm_argo_application_wait
+  to   = module.crds.kubernetes_role_binding.helm_argo_application_wait
+}
+
+moved {
+  from = kubernetes_service_account.crds_helm_argo_application_wait
+  to   = module.crds.kubernetes_service_account.helm_argo_application_wait
+}
+
+moved {
   from = helm_release.control_plane_argo_application
   to   = module.addon.helm_release.argo_application
 }
@@ -26,4 +46,24 @@ moved {
 moved {
   from = helm_release.control_plane
   to   = module.addon.helm_release.this
+}
+
+moved {
+  from = kubernetes_job.helm_argo_application_wait
+  to   = module.addon.kubernetes_job.helm_argo_application_wait
+}
+
+moved {
+  from = kubernetes_role.helm_argo_application_wait
+  to   = module.addon.kubernetes_role.helm_argo_application_wait
+}
+
+moved {
+  from = kubernetes_role_binding.helm_argo_application_wait
+  to   = module.addon.kubernetes_role_binding.helm_argo_application_wait
+}
+
+moved {
+  from = kubernetes_service_account.helm_argo_application_wait
+  to   = module.addon.kubernetes_service_account.helm_argo_application_wait
 }
