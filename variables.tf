d5ab62414f719d95e63f654de20de43a2b0a2597
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2026-01-15 21:20:48 +0100

    feat(crds): add crds_depends_on variable and update related configurations

--- variables.tf
+++ variables.tf
@@ -1,6 +1,1 @@
 # IMPORTANT: Add addon specific variables here
-variable "crds_enabled" {
-  type        = bool
-  default     = true
-  description = "Set to false to prevent the module from creating CRD resources."
-}
