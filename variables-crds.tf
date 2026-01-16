b235c80896cc97a6b44cb5837911ff4dfcd2c9ef
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2026-01-16 12:57:10 +0100

    feat(addon)!: add support for addon depends on (#20)
    # Description
    
    <!--
    Please include a summary of the change, provide a justification and
    which issue is fixed.
    -->
    
    :warning: **[BREAKING]** In this PR addon module version is bumped to
    0.0.25 to include the new feature in the addon. The most significant
    change is Terraform 1.8 requirement.
    
    Additionally, this PR adds variable *addon_depends_on* to have more
    granular control over addon dependencies preventing Terraform plan to
    trigger change when data source changes.
    
    ## Type of change
    
    - [ ] A bug fix (PR prefix `fix`)
    - [x] A new feature (PR prefix `feat`)
    - [ ] A code change that neither fixes a bug nor adds a feature (PR
    prefix `refactor`)
    - [ ] Adding missing tests or correcting existing tests (PR prefix
    `test`)
    - [ ] Changes that do not affect the meaning of the code like
    white-spaces, formatting, missing semi-colons, etc. (PR prefix `style`)
    - [ ] Changes to our CI configuration files and scripts (PR prefix `ci`)
    - [ ] Documentation only changes (PR prefix `docs`)
    
    ## How Has This Been Tested?
    
    <!--
    Please describe the tests that you ran to verify your changes.
    -->
    
    Checked when *addon_depends_on* is used instead of *depends_on* if the
    Terraform plan stays the same when dependency changes.

--- variables-crds.tf
+++ variables-crds.tf
@@ -1,4 +1,8 @@
-# IMPORTANT: This file is synced with the "terraform-aws-eks-universal-addon" module. Any changes to this file might be overwritten upon the next release of that module.
+variable "crds_enabled" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating CRD resources."
+}
 
 variable "crds_helm_enabled" {
   type        = bool
@@ -376,4 +380,10 @@ variable "crds_helm_postrender" {
   type        = map(any)
   default     = null
   description = "Value block with a path to a binary file to run after Helm renders the manifest which can alter the manifest contents. Defaults to `{}`."
+}
+
+variable "crds_depends_on" {
+  type        = any
+  default     = null
+  description = "List of resources to wait for before installing the addon. Typically used to force a dependency on another addon."
 }
